213b2b716cc5e64ba8ac5c57209c0574ede5b55b
SQC <798831181@qq.com>
2019-10-03 13:34:38 +0800

    delete

---
+++ input-center/src/main/java/com/sqc/center/service/CategoryServiceImpl.java
@@ -0,0 +1,12 @@
+package com.sqc.center.service;
+
+import com.sqc.api.service.CategoryService;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class CategoryServiceImpl implements CategoryService {
+
+    public void test(){
+
+    }
+}
